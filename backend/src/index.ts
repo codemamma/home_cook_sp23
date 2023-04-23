fe8e57c1b26f08c22812dc0f11317bd21aad6e30
codemamma <manishay1985@gmail.com>
2023-04-22 21:22:54 -0400

    fastify in index.ts, hello3 server connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
+//Add Fastify
+//Add database
+
 import dotenv from "dotenv";
-import app from "./app.js";
+//import app from "./app.js";
+import Fastify from "fastify";
 dotenv.config();
+
+const app=Fastify();
+app.get("/hello3",async (req,res,) =>{
+	return 'hello3';
+});
 
 app.listen({ port: 8080}, 
 	(err, address) => {
